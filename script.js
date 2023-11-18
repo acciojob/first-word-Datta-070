2b88c777e6dc5197354a49fe47891b0c2ea080a0
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-18 11:44:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	s=s.split(" ");
+	console.log(s);
+	for(let i=0;i<s.length;i++){
+		if(s[i] != ""){
+			return s[i];
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
